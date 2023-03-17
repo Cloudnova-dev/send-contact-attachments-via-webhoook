3b2faf567504ef518cdb2e8bbf67415e36f3aacb
Cloudnova DEV <103260042+Cloudnova-dev@users.noreply.github.com>
2023-03-17 17:46:13 +0100

    Update custom-action.js

--- custom-action.js
+++ custom-action.js
@@ -9,7 +9,7 @@ const hubspot = require('@hubspot/api-client');
 const hubspotClient = new hubspot.Client({ accessToken: process.env.HAPIKEY });
 
 exports.main = async (event, callback) => {
-  const email = event.inputFields['hs_server_response'];
+  const webhookBody = event.inputFields['hs_server_response'].json();
   const contactId = event.inputFields['hs_object_id'];
   const apiKey = process.env.HAPIKEY;
   // console.log(email);
@@ -20,9 +20,14 @@ exports.main = async (event, callback) => {
   
   
   fetch(`https://api.hubapi.com/crm-associations/v1/associations/${contactId}/HUBSPOT_DEFINED/9`, { headers: headers })
-    .then(response => response.json())
+    .then(response => response.json()) /*
+    response example
+    { "results": [ 30456250483, 30542931549, 30942006137, 31303812866, 32598911865 ], "hasMore": false, "offset": 32598911865 }
+    */
     .then(data => {
-      // to be done
+      // to be done: 
+      // query each engagement for attachments
+      // get all attachments IDs to send to C4C
       
     })
     .catch(error => {
