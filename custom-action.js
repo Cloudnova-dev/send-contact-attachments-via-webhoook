6cb9f5887d03f1277459eaf1e26964c049b69e98
Cloudnova DEV <103260042+Cloudnova-dev@users.noreply.github.com>
2023-03-20 11:51:04 +0100

    Update custom-action.js

--- custom-action.js
+++ custom-action.js
@@ -5,10 +5,10 @@
 * and add them with their ID at the end of the quey body
 ****************/
 
-const hubspot = require('@hubspot/api-client');
-const hubspotClient = new hubspot.Client({ accessToken: process.env.HAPIKEY });
+// const hubspot = require('@hubspot/api-client');
+// const hubspotClient = new hubspot.Client({ accessToken: process.env.HAPIKEY });
 
-exports.main = async (event, callback) => {
+exports.main = async (event) => {
   const webhookBody = event.inputFields['hs_server_response'].json();
   const contactId = event.inputFields['hs_object_id'];
   const apiKey = process.env.HAPIKEY;
@@ -18,28 +18,21 @@ exports.main = async (event, callback) => {
     'Content-Type': 'application/json'
   };
   
-  
-  fetch(`https://api.hubapi.com/crm-associations/v1/associations/${contactId}/HUBSPOT_DEFINED/9`, { headers: headers })
-    .then(response => response.json()) /*
-    response example
+  const attachments = [];
+  const engagements = fetch(`https://api.hubapi.com/crm-associations/v1/associations/${contactId}/HUBSPOT_DEFINED/9`, { headers: headers }).then(response => response.json());
+    /* response example
     { "results": [ 30456250483, 30542931549, 30942006137, 31303812866, 32598911865 ], "hasMore": false, "offset": 32598911865 }
     */
-    .then(data => {
-      // to be done: 
-      // query each engagement for attachments
-      // get all attachments IDs to send to C4C
-      
-    })
-    .catch(error => {
-      // Log any errors to the console
-      console.error(error);
+    await engagements.results.forEach(engagement => {
+      fetch(`https://api.hubapi.com/engagements/v1/engagements/${engagement}`, { headers: headers })
+      //     { "engagement": { "id": 30456250483, "portalId": 23816749, "active": true, "createdAt": 1674661824904, "lastUpdated": 1674661824904, "createdBy": 49259232, "modifiedBy": 49259232, "ownerId": 311325221, "type": "NOTE", "timestamp": 1674661824904, "allAccessibleTeamIds": [], "queueMembershipIds": [], "bodyPreviewIsTruncated": false }, "associations": { "contactIds": [ 51 ], "companyIds": [], "dealIds": [], "ownerIds": [], "workflowIds": [], "ticketIds": [], "contentIds": [], "quoteIds": [], "marketingEventIds": [] }, 
+      //     "attachments": [ { "id": 99876123753 } ], "metadata": {} }
+      .then(data =>{
+        if (data.attachments.lenght != 0) {
+          attachments.push(data.attachments);
+        }
+      })
     });
 
-  callback({
-    outputFields: {
-      email: email
-    }
-  });
+    webhookBody.attachments = attachments;
 }
-
-
